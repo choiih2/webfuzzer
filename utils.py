613efa4db30067bab836d0a1fa95a5f7b5265fee
choiih2 <132427206+choiih2@users.noreply.github.com>
2026-08-06 19:00:05 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-# core/utils.py
-from urllib.parse import urlparse
-
-def get_endpoint_string(url):
-    parsed = urlparse(url)
-    endpoint = parsed.path or "/"
-    if parsed.query:
-        endpoint += "?" + parsed.query
-    return endpoint
